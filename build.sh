5aaea6abc912b32811a876a6bf99b3f77bb1289b
Oleg Valter <o.a.philippov@gmail.com>
2023-06-10 23:40:31 +0300

    split dark mode client-side JS from dashboard-related JS

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ node-sass public/stylesheets --output build/public/stylesheets &&
 tsc -t es5 --lib es7 --outDir build --esModuleInterop ./*.ts &&
 echo "> mkdir -p build/public/javascripts" &&
 mkdir -p build/public/javascripts &&
-echo "> uglifyjs public/javascripts/*.js -c --source-map -o build/public/javascripts/application.js" &&
-uglifyjs public/javascripts/*.js -c --source-map -o build/public/javascripts/application.js &&
+echo "> uglifyjs public/javascripts/application.js -c --source-map -o build/public/javascripts/application.js" &&
+uglifyjs public/javascripts/application.js -c --source-map -o build/public/javascripts/application.js &&
+echo "> uglifyjs public/javascripts/dark.js -c --source-map -o build/public/javascripts/dark.js" &&
+uglifyjs public/javascripts/dark.js -c --source-map -o build/public/javascripts/dark.js &&
 echo "> cp -r public/stylesheets build/public" &&
 cp -r public/stylesheets build/public &&
 cp public/icon.png build/public/icon.png &&
