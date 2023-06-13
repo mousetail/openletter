85e9a7bb8f90cb1e0719689c23a9cd208bf07633
double beep <38133098+double-beep@users.noreply.github.com>
2023-06-13 07:43:06 +0000

    improve readability

--- app.ts
+++ app.ts
@@ -54,12 +54,27 @@ for (const [path, routerFactory] of Object.entries(routes)) {
 // Handle errors
 app.use((req: express.Request, res: ResponseWithLayout) => {
     if (res.statusCode === 500) {
-        render(req, res, 'common/coded_err', { name: 'Server Error',
-            description: 'The server encountered an internal error while serving your request.' },
-        { pool });
+        render(
+            req,
+            res,
+            'common/coded_err',
+            {
+                name: 'Server Error',
+                description: 'The server encountered an internal error while serving your request.'
+            },
+            { pool }
+        );
     } else {
-        render(req, res, 'common/coded_err', { name: 'Not Found', description: 'The page you requested could not be found.' },
-            { pool });
+        render(
+            req,
+            res,
+            'common/coded_err',
+            {
+                name: 'Not Found',
+                description: 'The page you requested could not be found.'
+            },
+            { pool }
+        );
     }
 });
 
